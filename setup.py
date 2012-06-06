7842da861c389d0264e7a830b296181ba146ff13
Sean O'Connor <sean@seanoc.com>
2012-06-06 14:29:32 -0400

    Added requests as a dependency.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     long_description=open('README.md').read(),
     author="Sean O'Connor",
     author_email="sean@focuslab.io",
+    install_requires=['requests>=0.13.0'],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
